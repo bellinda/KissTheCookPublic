b0d500b6122be475649b678ce3c6a28510fcfa49
Gabriela Angelova <gabriela.angelova@fdiba.tu-sofia.bg>
2014-11-25 11:14:44 +0200

    new

---
+++ KissTheCookCustomEdition/KissTheCookCustomEdition.WindowsPhone/Pages/NoTimePage.xaml.cs
@@ -0,0 +1,160 @@
+﻿using KissTheCookCustomEdition.Common;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Graphics.Display;
+using Windows.UI.Notifications;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Basic Page item template is documented at http://go.microsoft.com/fwlink/?LinkID=390556
+
+namespace KissTheCookCustomEdition.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class NoTimePage : Page
+    {
+        private NavigationHelper navigationHelper;
+        private ObservableDictionary defaultViewModel = new ObservableDictionary();
+
+        public NoTimePage()
+        {
+            this.InitializeComponent();
+
+            this.navigationHelper = new NavigationHelper(this);
+            this.navigationHelper.LoadState += this.NavigationHelper_LoadState;
+            this.navigationHelper.SaveState += this.NavigationHelper_SaveState;
+        }
+
+        /// <summary>
+        /// Gets the <see cref="NavigationHelper"/> associated with this <see cref="Page"/>.
+        /// </summary>
+        public NavigationHelper NavigationHelper
+        {
+            get { return this.navigationHelper; }
+        }
+
+        /// <summary>
+        /// Gets the view model for this <see cref="Page"/>.
+        /// This can be changed to a strongly typed view model.
+        /// </summary>
+        public ObservableDictionary DefaultViewModel
+        {
+            get { return this.defaultViewModel; }
+        }
+
+        /// <summary>
+        /// Populates the page with content passed during navigation.  Any saved state is also
+        /// provided when recreating a page from a prior session.
+        /// </summary>
+        /// <param name="sender">
+        /// The source of the event; typically <see cref="NavigationHelper"/>
+        /// </param>
+        /// <param name="e">Event data that provides both the navigation parameter passed to
+        /// <see cref="Frame.Navigate(Type, Object)"/> when this page was initially requested and
+        /// a dictionary of state preserved by this page during an earlier
+        /// session.  The state will be null the first time a page is visited.</param>
+        private void NavigationHelper_LoadState(object sender, LoadStateEventArgs e)
+        {
+        }
+
+        /// <summary>
+        /// Preserves state associated with this page in case the application is suspended or the
+        /// page is discarded from the navigation cache.  Values must conform to the serialization
+        /// requirements of <see cref="SuspensionManager.SessionState"/>.
+        /// </summary>
+        /// <param name="sender">The source of the event; typically <see cref="NavigationHelper"/></param>
+        /// <param name="e">Event data that provides an empty dictionary to be populated with
+        /// serializable state.</param>
+        private void NavigationHelper_SaveState(object sender, SaveStateEventArgs e)
+        {
+        }
+
+        #region NavigationHelper registration
+
+        /// <summary>
+        /// The methods provided in this section are simply used to allow
+        /// NavigationHelper to respond to the page's navigation methods.
+        /// <para>
+        /// Page specific logic should be placed in event handlers for the  
+        /// <see cref="NavigationHelper.LoadState"/>
+        /// and <see cref="NavigationHelper.SaveState"/>.
+        /// The navigation parameter is available in the LoadState method 
+        /// in addition to page state preserved during an earlier session.
+        /// </para>
+        /// </summary>
+        /// <param name="e">Provides data for navigation methods and event
+        /// handlers that cannot cancel the navigation request.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            this.navigationHelper.OnNavigatedTo(e);
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            this.navigationHelper.OnNavigatedFrom(e);
+        }
+
+        #endregion
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int hours = 0;
+            int minutes = 0;
+            if (int.TryParse(this.availableHoursTextBox.Text, out hours) == false || int.TryParse(this.availableMinutesTextBox.Text, out minutes) == false)
+            {
+                SendNotification("Invalid input", "No empty fields are", "possible", "/Images/input.png");
+            }
+            else
+            {
+                if (hours > 23 || hours < 0)
+                {
+                    SendNotification("Invalid input", "Hours should be between ", "0 and 23", "/Images/input.png");
+                    this.availableHoursTextBox.Text = "";
+                }
+                else if (minutes < 0 || minutes > 59)
+                {
+                    SendNotification("Invalid input", "Minutes should be between ", "0 and 59", "/Images/input.png");
+                    this.availableMinutesTextBox.Text = "";
+                }
+                else
+                {
+                    int allMinutes = hours * 60 + minutes;
+                    this.Frame.Navigate(typeof(Pages.FoundRecipesByTime), allMinutes);
+                }
+
+            }
+        }
+
+        private static void SendNotification(string mainMessage, string secondMessage, string thirdMessage, string imageSrc)
+        {
+            var notificationXml = ToastNotificationManager.GetTemplateContent(ToastTemplateType.ToastImageAndText04);
+            string toastXmlString = "<toast>"
+                               + "<visual version='1'>"
+                               + "<binding template='toastImageAndText04'>"
+                               + "<text id='1'>" + mainMessage + "</text>"     //No internet connection!
+                               + "<text id='2'>" + secondMessage + "</text>" //Turn it on to see 
+                               + "<text id='3'>" + thirdMessage + "</text>"   //a list of restorants near you
+                               + "<image id='1' src='" + imageSrc + "' alt='image placeholder'/>"
+                               + "</binding>"
+                               + "</visual>"
+                               + "</toast>";
+            notificationXml.LoadXml(toastXmlString);         //var toeastElement = notificationXml.GetElementsByTagName("text");
+            //toeastElement[0].AppendChild(notificationXml.CreateTextNode("This is Notification Message"));
+            var toastNotification = new ToastNotification(notificationXml);
+            ToastNotificationManager.CreateToastNotifier().Show(toastNotification);
+        }
+    }
+}
